068d344c61ba66f22640dba80597354b2fd98535
Jargalsaikhan Agiimaa <agiimaa12@Jargalsaikhans-MacBook-Pro.local>
2024-01-11 18:48:07 +0800

    first commit

---
+++ routes/factRoute.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const factRouter = Router()
+
+// const { createUser, loginUser } = require('../controller/userController');
+const FactModel = require('../database/schema/factSchema')
+const { postFact, getFact, deleteFact, putFact, addLikes, disLikes } = require('../controller/factController')
+// const { readFact } = require('../controller/factController')
+
+
+factRouter.post('/facts', postFact)
+factRouter.get('/facts', getFact)
+factRouter.delete('/facts/:factID', deleteFact)
+factRouter.put('/facts/:factID', putFact)
+factRouter.post('/addLikes/:factID/:userID', addLikes)
+factRouter.post ('/disLikes/:factID/:userID', disLikes)
+
+module.exports = factRouter
+
+
